de481ee9e7a2535c93278ea7e17cf8a428d16e26
Francesco Tullii <francesco.tullii@fcagroup.com>
2020-04-17 16:04:09 +0200

    base version fixed

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -44,7 +44,7 @@ class WaypointUpdater(object):
         self.waypoints_2d = None
         self.waypoints_tree = None
 
-        self.rate = rospy.rate(50)
+        self.rate = rospy.Rate(50)
         self.loop()
 
     def loop(self):
@@ -86,7 +86,7 @@ class WaypointUpdater(object):
     def waypoints_cb(self, waypoints):
         self.base_waypoints = waypoints
         if not self.waypoints_2d:
-            self.waypoints_2d = [[waypoint.pose.pose.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
