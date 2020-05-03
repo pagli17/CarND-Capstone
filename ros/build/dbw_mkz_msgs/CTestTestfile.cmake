d038d60be32a1f2384c250f25885038c8f9a0698
Alessandro Paglialonga <alessandro.paglialonga17@gmail.com>
2020-05-03 17:30:52 +0000

    Deleted ignored cached files, changed rates

--- ros/build/dbw_mkz_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/CarND-Capstone/ros/src/dbw_mkz_msgs
-# Build directory: /home/workspace/CarND-Capstone/ros/build/dbw_mkz_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -24,7 +24,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 MAX_DECEL = 1.
 
 
@@ -51,7 +51,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(15)
         while not rospy.is_shutdown():
             if self.pose and self.base_waypoints and self.waypoint_tree:
                 self.publish_waypoints()
